1a4e08f792af8ffa7c9615ea1c1fcd6f5eb52bb4
Mhmd <biboo.biboo97@gmail.com>
2021-11-17 21:51:43 +0200

    Social Media Dashboard

---
+++ README.md
@@ -0,0 +1,20 @@
+# Frontend Mentor - Social media dashboard with theme switcher solution
+
+This is a solution to the [Social media dashboard with theme switcher challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Links
+
+- Solution [URL](https://www.frontendmentor.io/solutions/social-media-dashboard-with-theme-switcher-)
+- Live Site [URL](https://mhmd-tarek-mhmd.github.io/Social-Media-Dashboard/)
+
+## Screenshots
+
+### Light Theme
+
+![](screenshots/light-desktop.png)
+![](screenshots/light-mobile.png)
+
+### Dark Theme
+
+![](screenshots/dark-desktop.png)
+![](screenshots/dark-mobile.png)
---
+++ js/app.js
@@ -0,0 +1,184 @@
+const $ = document.querySelector.bind(document);
+
+(function () {
+  const Model = {
+    accounts: {
+      facebook: {
+        name: "@nathanf",
+        totalFollowers: 1987,
+        todayFollowers: 12,
+      },
+      twitter: {
+        name: "@nathanf",
+        totalFollowers: 1044,
+        todayFollowers: 99,
+      },
+      instagram: {
+        name: "@realnathanf",
+        totalFollowers: "11k",
+        todayFollowers: 1099,
+      },
+      youtube: {
+        name: "Nathan F.",
+        totalFollowers: 8239,
+        todayFollowers: -144,
+      },
+    },
+
+    todayOverview: {
+      facebook: {
+        pageViews: 87,
+        viewsOverview: 3,
+        likes: 52,
+        likesOverview: -2,
+      },
+      instagram: {
+        likes: 5462,
+        likesOverview: 2257,
+        profileViews: "52k",
+        viewsOverview: 1375,
+      },
+      twitter: {
+        retweets: 117,
+        retweetsOverview: 303,
+        likes: 507,
+        likesOverview: 553,
+      },
+      youtube: {
+        likes: 107,
+        likesOverview: -19,
+        totalViews: 1407,
+        viewsOverview: -12,
+      },
+    },
+  };
+
+  const Controller = {
+    getAccounts: () => Object.entries(Model.accounts),
+    getTodayOverview: () => Object.entries(Model.todayOverview),
+
+    init: function () {
+      Views.init();
+    },
+  };
+
+  const Views = {
+    init: function () {
+      /*
+       ** Accounts
+       */
+      Controller.getAccounts().forEach((account) => {
+        const platformName = account[0];
+        const accounts = account[1];
+
+        $(".accounts").innerHTML += Components.accounts(platformName, accounts);
+      });
+
+      /*
+       ** Today Overview
+       */
+
+      Controller.getTodayOverview().forEach((overview) => {
+        const platformName = overview[0];
+        const overviewProperties = overview[1];
+
+        $(".overview .cards").innerHTML += Components.overview(
+          platformName,
+          overviewProperties
+        );
+      });
+
+      /*
+       ** Theme Toggler
+       */
+      $("#theme-toggler").onclick = (e) => {
+        $("html").classList.contains("light")
+          ? ($("html").className = "dark")
+          : ($("html").className = "light");
+      };
+    },
+  };
+
+  const Components = {
+    accounts: function (platformName, account) {
+      const name = account.name;
+      const todayFollowers = account.todayFollowers;
+      const totalFollowers = account.totalFollowers;
+
+      return `
+          <div class='card ${platformName}'>
+            <div class='name center-flex'>
+              <img src="images/icon-${platformName}.svg" alt="${platformName} icon" />
+              <span>${name}</span>
+            </div>
+
+            <div class='total-followers'>
+              <h3>${totalFollowers}</h3>
+              <span>
+                ${platformName === "youtube" ? "Subscribes" : "Followers"}
+              </span>
+            </div>
+
+            <div class='today-followers center-flex ${
+              todayFollowers < 0 ? "decrease" : "increase"
+            }'>
+              <img src="images/icon-${
+                todayFollowers < 0 ? "down" : "up"
+              }.svg" alt="" />
+              <span>${Math.abs(todayFollowers)}</span> Today
+            </div>
+          </div>
+        `;
+    },
+
+    overview: function (platformName, props) {
+      const overviewPropertiesNames = Object.getOwnPropertyNames(props);
+      const firstPropertyName = overviewPropertiesNames[0];
+      const secondPropertyName = overviewPropertiesNames[1];
+      const thirdPropertyName = overviewPropertiesNames[2];
+      const forthPropertyName = overviewPropertiesNames[3];
+
+      return `
+          <div class="card d-grid">
+            <div class='space-between-flex'>
+              <span>${firstPropertyName.replace(/([A-Z])/g, " $1")}</span>
+              <img src="images/icon-${platformName}.svg" alt="${platformName} icon" />
+            </div>
+
+            <div class='space-between-flex'>
+              <h4>${props[firstPropertyName]}</h4>
+              <div class='center-flex ${
+                props[secondPropertyName] < 0 ? "decrease" : "increase"
+              }'>
+                <img src="images/icon-${
+                  props[secondPropertyName] < 0 ? "down" : "up"
+                }.svg" alt="" />
+                <span>${Math.abs(props[secondPropertyName])}</span> %
+              </div>
+            </div>
+          </div>
+
+          <div class="card d-grid">
+            <div class='space-between-flex'>
+              <span>${thirdPropertyName.replace(/([A-Z])/g, " $1")}</span>
+              <img src="images/icon-${platformName}.svg" alt="${platformName} icon" />
+            </div>
+
+            <div class='space-between-flex'>
+              <h4>${props[thirdPropertyName]}</h4>
+              <div class='center-flex ${
+                props[forthPropertyName] < 0 ? "decrease" : "increase"
+              }'>
+                <img src="images/icon-${
+                  props[forthPropertyName] < 0 ? "down" : "up"
+                }.svg" alt="" />
+                <span>${Math.abs(props[forthPropertyName])}</span> %
+              </div>
+            </div>
+          </div>
+        `;
+    },
+  };
+
+  Controller.init();
+})();
